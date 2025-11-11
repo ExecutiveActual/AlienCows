5f2064af37ee61f094e5e639c310f2833330c419
ExecutiveActual <ihaveacareer@gmail.com>
2025-11-11 08:11:10 -0600

    Add UnityEvent for scene change in GameManager
    Usage:
    GameManager_Singleton.Instance.GetComponent<GameManager_SceneChangeEvents>().UE_OnChangeScene?.Invoke("Scene Name");

--- Assets/Custom/Scripts/GameManagement/GameManager_SceneChangeEvents.cs
+++ Assets/Custom/Scripts/GameManagement/GameManager_SceneChangeEvents.cs
@@ -1,16 +1,9 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class GameManager_SceneChangeEvents : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public UnityEvent<string> UE_OnChangeScene;
+
 }
