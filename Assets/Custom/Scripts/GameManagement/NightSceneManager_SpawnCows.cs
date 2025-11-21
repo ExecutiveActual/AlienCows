d86f8678b5480399db30709c38580e4a3728c729
ExecutiveActual <ihaveacareer@gmail.com>
2025-11-21 15:06:18 -0600

    NightManager Spawns the Saved Amount of Cows

--- Assets/Custom/Scripts/GameManagement/NightSceneManager_SpawnCows.cs
+++ Assets/Custom/Scripts/GameManagement/NightSceneManager_SpawnCows.cs
@@ -5,17 +5,70 @@ public class NightSceneManager_SpawnCows : MonoBehaviour
 {
 
 
+    [SerializeField] private GameObject cowPrefab;
 
-    [SerializeField] private List<Transform> cowSpawnPoints;
+    [SerializeField] private Transform SpawnPointParent;
+
+    [SerializeField] private float spawnRandomSpread = 5f;
+
+    private Transform[] cowSpawnPoints;
+
+
+
+    private void Awake()
+    {
+        cowSpawnPoints = SpawnPointParent.GetComponentsInChildren<Transform>();
+    }
 
 
     public void SpawnCows(int amount)
     {
 
+        for (int i = 0; i < amount; i++)
+        {
+
+            Transform spawnPoint = GetRandomSpawnPoint();
+            Instantiate(cowPrefab, GetRandomPos(spawnPoint.position, spawnRandomSpread), GetRandomRot());
+
+            Debug.Log($"spawned Cow i = {i}");
+        }
 
         Debug.Log($"Spawned {amount} cows");
 
 
+    }
+
+    private Transform GetRandomSpawnPoint()
+    {
+        List<Transform> possibleSpawnPoints = new List<Transform>(cowSpawnPoints);
+        int randomIndex = Random.Range(0, possibleSpawnPoints.Count);
+        return possibleSpawnPoints[randomIndex];
+    }
+
+
+    private Vector3 GetRandomPos(Vector3 originalPos, float spread)
+    {
+        Vector3 pos = originalPos;
+        pos.x += Random.Range(-spread, spread);
+        pos.z += Random.Range(-spread, spread);
+        return pos;
+    }
+
+    private Quaternion GetRandomRot()
+    {
+        return Quaternion.Euler(0, Random.Range(0, 360), 0);
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        if (SpawnPointParent == null) return;
+        Transform[] spawnPoints = SpawnPointParent.GetComponentsInChildren<Transform>();
+        Gizmos.color = Color.green;
+        foreach (Transform spawnPoint in spawnPoints)
+        {
+            Gizmos.DrawSphere(spawnPoint.position, 0.2f);
+        }
     }
 
 }
