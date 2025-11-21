9c1888cffa69c5c860fd25df6013aa3d64107a75
ExecutiveActual <ihaveacareer@gmail.com>
2025-11-21 16:22:02 -0600

    Refactor NightSceneManager structure and add WaveGenerator

---
+++ Assets/Custom/Scripts/GameManagement/WaveGenerator.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveGenerator : MonoBehaviour
+{
+
+
+
+
+    private WaveData GenerateWave()
+    {
+
+        WaveData newWave = new WaveData();
+
+
+
+
+
+        return newWave;
+    }
+
+
+
+
+
+}
+
+public struct WaveData
+{
+
+    List<string> enemySpawnOrder_Names;
+
+}
