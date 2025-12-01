b95fa54a22df6e07ed4591272455ad06009ab0d1
ExecutiveActual <ihaveacareer@gmail.com>
2025-12-01 08:42:14 -0600

    CombatRifle rebalance; Cow spawn bug fix; WIP sound volume fix

---
+++ Assets/Scripts/ScriptableObjects/SO_SoundVolumeSetting.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SO_SoundVolumeSetting", menuName = "Scriptable Objects/SO_SoundVolumeSetting")]
+public class SO_SoundVolumeSetting : ScriptableObject
+{
+    
+    public float volume_Curr = 0.8f;
+
+}
